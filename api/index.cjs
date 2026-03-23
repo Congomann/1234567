e752226763a3d1bdb5b5a50f969f2a281fb19b9f
Congomann <info@newhollandfinancial.com>
2026-03-23 00:42:39 -0500

    Remove conflicting api/index.cjs to fix Vercel build

--- api/index.cjs
+++
@@ -1,6 +0,0 @@
-const app = require('../backend/server.cjs');
-
-// Vercel handles the listening part for us, but our server.cjs
-// has a server.listen() block at the bottom. 
-// When required as a module, we just need to export the app.
-module.exports = app;
