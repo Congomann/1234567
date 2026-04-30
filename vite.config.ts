48515379b9eb25a1957bf916764f5d6a16969103
Congomann <info@newhollandfinancial.com>
2026-04-30 14:52:33 -0500

    fix: update local vite proxy to point to correct backend port

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
         },
         proxy: {
           '/api': {
-            target: `http://127.0.0.1:3122`,
+            target: `http://127.0.0.1:3001`,
             changeOrigin: true,
             secure: false,
           },
